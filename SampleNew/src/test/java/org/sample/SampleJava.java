1f6f5449ff42328c1cc170a6e2a0fb190fce4d7a
Prasanna <Prasanna@Prasan>
2023-11-05 13:32:22 +0530

    Added dependency and browser launch code

--- SampleNew/src/test/java/org/sample/SampleJava.java
+++ SampleNew/src/test/java/org/sample/SampleJava.java
@@ -1,9 +1,16 @@
 package org.sample;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SampleJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
 
 	}
 
